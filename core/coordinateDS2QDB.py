12ee8e2eeec4d68bf345e9c7bea6230bd536e82f
liuruheng <liuruheng@seuic.com>
2017-10-24 15:04:00 +0800

    数据抓取和行情数据库耦合模块逻辑优化

--- core/coordinateDS2QDB.py
+++ core/coordinateDS2QDB.py
@@ -4,35 +4,33 @@ import sys
 from engine.DataScrape import *
 from timer.TimerMotor import *
 from database.QuotationDB import *
+from resource import Configuration
 
 class coordinateDS2QDB():
     def __init__(self):
         self.dumpFlag = True
 
     def init_data_scrape(self):
+        """ 初始化数据抓取模块 """
         self.dtScrp = DataScrape()
-        self.infoTuple = None#数据筛选后列表（待缓冲字典处理）
 
     def init_quotation_db(self):
         """ 行情数据库线程准备 """
         # 对应数据库准备
-        self.dbQuotationDBHdl = QuotationDB()#Quotation DB Handle
-        #创建缓冲记录字典
+        self.dbQuotationDBHdl = QuotationDB() # Quotation DB Handle
+        # 创建记录字典
         self.dbQuotationDBHdl.create_record_dict()
-        #Quotation DB file name(with path)
-        self.dbQuotationDBHdl.create_file()
+        # 创建行情数据库文件
+        self.dbQuotationDBHdl.create_period_db_file()
 
     # 以下是定时器回调函数:
-    def work_QDB_update(self):
-        """ 慢速定时器回调函数 : 更新行情数据库 """
-        self.dbQuotationDBHdl.db_quotation_insert(self.infoTuple)
+    def work_DS2QDB_heartbeat(self):
+        """ 快速定时器(心跳定时器)回调函数 : 数据抓取模块和行情数据库线程(缓冲字典)之间协同工作函数 """
+        # 数据抓取并筛选
+        infoTuple = self.dtScrp.query_info()
+        if self.dumpFlag: print infoTuple
+        self.dbQuotationDBHdl.update_dict_record(infoTuple)
 
-    def work_DS2QDB_record(self):
-        """ 快速定时器回调函数 : 数据抓取模块和行情数据库线程(缓冲字典)之间协同工作函数 """
-        #数据抓取并筛选
-        self.infoTuple = self.dtScrp.query_info()
-        if self.dumpFlag: print self.infoTuple
-        self.dbQuotationDBHdl.update_record_dict(self.infoTuple)
-
-    def idiot(self):
-        print "idiot"
+    def work_DS2QDB_operate(self):
+        """ 慢速定时器组回调函数 : 更新行情数据库 """
+        self.dbQuotationDBHdl.update_period_db()
