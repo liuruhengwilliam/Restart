e8f1d44a757ff1c5d021fcb13d0cd6f1ca4cf27c
liuruheng <liuruheng@seuic.com>
2017-10-24 17:14:32 +0800

    使用py2exe工具发布alpha版本V0.1.0

---
+++ core/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+import py2exe
+setup(
+    version='0.1.0',
+    description='alpha for Quotaion DB. Marked by liuruheng on 2017-10-24',
+    name = 'alpha V0.1.0',
+    console=['__main__.py'])
