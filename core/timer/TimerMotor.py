4b2a9ccd9026cc3acaa6f01a2f701205a920671c
liuruheng <liuruheng@seuic.com>
2017-10-24 14:58:41 +0800

    定时器引擎模块简化：固定周期定期器组和链式定时器两种定时器

--- core/timer/TimerMotor.py
+++ core/timer/TimerMotor.py
@@ -5,52 +5,52 @@ import datetime
 from LoopTimer import LoopTimer
 from IntervalTimer import IntervalTimer
 
-#循环定时器周期
-#数据抓取两级定时器：快周期和慢周期定时器
-FAST_LOOP_PERIOD = 10 #5 second
-SLOW_LOOP_PERIOD = 300 #5 minute
-
-#链式定时器周期
-CHAIN_PERIOD = [1800,1*3600-1800,2*3600-1*3600,4*3600-2*3600,6*3600-4*3600,\
-                12*3600-6*3600,24*3600-12*3600,24*5*3600-24*3600]
-
 class TimerMotor():
     """ 定时器引擎 """
-    def __init__(self, fastTimerFunc, slowTimerFunc, chainTimerFunc):
-        """ 初始化三种定时器（链）"""
-        self.fastTimer = self.slowTimer = self.chainTimer = None
-        self.fastTimerFunc = fastTimerFunc
-        self.slowTimerFunc = slowTimerFunc
-        self.chainTimerFunc = chainTimerFunc
+    def __init__(self):
+        """ 外部接口API:初始化三种定时器（链）。
+            cacheTimer和arrayTimer定时器具有相同定时逻辑：即同时启动一个（组）周期定时器。
+            chainTimer定时器逻辑：前一个定时器到期之后再去设置下一个定时器。"""
+
+        # fastenTimer是一组定时器。定时器的起始时间是相同的。
+        self.fastenTimer = []# 固定周期定时器列表
+        self.fastenTimerPeriod = [] # 固定周期定时器时间列表
+        self.fastenTimerFunc = [] # 固定周期定时器回调函数列表。回调函数可能不同。
+
+        # chainTimer是一个定时器。一个定时周期会变化的定时器。
+        self.chainTimer = None
+        self.chainTimerPeriod = [] # 链式周期定时器时间列表
+        self.chainTimerFunc = None
+
+    def init_fasten_timer(self, tmFunc , period):
+        """ 外部接口API:初始化一个（组）固定周期定时器。
+            timerFunc：为定时器回调函数列表
+            period：固定周期定时列表 """
+        self.fastenTimerFunc = tmFunc
+        self.fastenTimerPeriod = period
+
+    def init_chain_timer(self, timerFunc , period):
+        """ 外部接口API:初始化一个（组）固定周期定时器。
+            timerFunc：为定时器回调函数
+            period：链式周期定时列表 """
+        self.chainTimerFunc = timerFunc
+        self.chainTimerPeriod = period
 
     def start_timer(self):
-        """  启动定时器 """
-        self.fastTimer = LoopTimer(FAST_LOOP_PERIOD, self.fastTimerFunc)
-        self.fastTimer.start()
+        """  外部接口API:启动定时器 """
+        if len(self.fastenTimerPeriod) != 0:
+            for (period,func) in zip(self.fastenTimerPeriod, self.fastenTimerFunc):
+                tmpTimer = LoopTimer(period, func)
+                tmpTimer.start()
+                # 添加到固定周期定时器列表中
+                self.fastenTimer.append(tmpTimer)
 
-        self.slowTimer = LoopTimer(SLOW_LOOP_PERIOD, self.slowTimerFunc)
-        self.slowTimer.start()
+        if len(self.chainTimerPeriod) != 0:
+            self.chainTimer = IntervalTimer(0, self.chainTimerFunc,self.chainTimerPeriod)
+            self.chainTimer.start()
 
-        self.chainTimer = IntervalTimer(0, self.chainTimerFunc,CHAIN_PERIOD)
-        self.chainTimer.start()
-
-    def get_fast_timer(self):
-        return self.fastTimer
-
-    def get_fastTM_cbfunc_result(self):
-        """获取快速定时器回调函数返回值"""
-        return self.fastTimer.get_cb_func_result()
-
-    def get_slow_timer(self):
-        return self.slowTimer
-
-    def get_slowTM_cbfunc_result(self):
-        """获取慢速定时器回调函数返回值"""
-        return self.slowTimer.get_cb_func_result()
+    def get_fasten_timer(self):
+        return self.fastenTimer
 
     def get_chain_timer(self):
         return self.chainTimer
-
-    def get_chainTM_cbfunc_result(self):
-        """获取链式定时器回调函数返回值"""
-        return self.chainTimer.get_cb_func_result()
