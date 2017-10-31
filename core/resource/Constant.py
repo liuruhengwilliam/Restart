049146384b5bff560dfa5d45e5a4a853b648d8a1
liuruheng <liuruheng@seuic.com>
2017-10-31 16:36:42 +0800

    增加软件版本号和运行时间的显示

--- core/resource/Constant.py
+++ core/resource/Constant.py
@@ -9,13 +9,17 @@ CLOSING_END_TIME=6
 # 数据抓取模块应该设定在每周一凌晨6点开始启动。这样能够兼顾到各周期记录（不遗漏）。
 
 class Constant():
-    """ 行业相关术语等定义 """
+    """ 软件版本，行业相关术语等定义 """
     def __init__(self):
         return
 
+    def get_version_info(self):
+        """ 外部接口API: """
+        print "Version Code: V0.2.3\n"+"Build Time: %s"%time.strftime("%Y-%m-%d %H:%M",time.localtime())
+
     def is_closing_market(self):
-        """ 判断当前时间是否为闭市时间 """
-        # 每工作日凌晨4点到6点(不含4点整和6点整)为结算时间
+        """ 外部接口API:判断当前时间是否为闭市时间 """
+        # 每工作日凌晨5点到6点为结算时间
         hour = time.strftime("%H",time.localtime())
         if int(hour) > CLOSING_BEGIN_TIME and int(hour) < CLOSING_END_TIME:
             return True
