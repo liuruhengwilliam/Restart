31d6d39bf37d33a7b7f5ee39add126327809edfc
liuruheng <liuruheng@seuic.com>
2017-11-02 10:31:03 +0800

    日线和周线定时器周期时间校准

--- core/resource/Configuration.py
+++ core/resource/Configuration.py
@@ -11,7 +11,8 @@ QUOTATION_STRUCTURE = ('startPrice','realPrice','maxPrice','minPrice','time')
 #循环定时器周期
 # 行情数据库记录项周期: 6sec(不生成db文件),5min,15min,30min,1hour,2hour,4hour,6hour,12hour,1day,1week
 QUOTATION_DB_PREFIX = ('6sec','5min','15min','30min','1hour','2hour','4hour','6hour','12hour','1day','1week')
-QUOTATION_DB_PERIOD = (6,5*60,15*60,30*60,1*3600,2*3600,4*3600,6*3600,12*3600,1*24*3600,5*24*3600)
+QUOTATION_DB_PERIOD = (6,5*60,15*60,30*60,1*3600,2*3600,4*3600,6*3600,12*3600,(24-1)*3600,(5*24-1)*3600)
+# 日线和周线定时器周期时间调整（日线需减去每日的结算时间，周线需减去周六闭市时间差） 2017-11-02
 
 #链式定时器周期
 CHAIN_PERIOD = (1800,1*3600-1800,2*3600-1*3600,4*3600-2*3600,6*3600-4*3600,\
