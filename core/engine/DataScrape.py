c2ed8d40d4a39dca4d84dde9012271eccaeca42a
liuruheng <liuruheng@seuic.com>
2017-11-09 10:06:01 +0800

    行情数据抓取模块的代码优化

--- core/engine/DataScrape.py
+++ core/engine/DataScrape.py
@@ -2,29 +2,28 @@
 
 import sys
 import time
-from DataSource import *
 import requests
 import re
 import traceback
+import DataSource
 
 class DataScrape():
     """ 数据抓取类 """
     def __init__(self):
         self.dumpFlag = False
-        self.dataSource = DataSource()
+        self.url = DataSource.FX678_XAG_URL
 
     def query_info(self):
         """ 外部接口API：获取某网站相关信息 """
         #每个数据源采集的字典结构可能(基本都)会不同
         infoDict = {}
         retList = [] # 待返回的数据列表
-        url = self.dataSource.get_source()
 
         #每个网站采集到的信息格式不同，需要加以区分
-        if url == self.dataSource.FX678_XAG_URL:
+        if self.url == DataSource.FX678_XAG_URL:
             try:
                 #数据请求
-                r = requests.get(url)
+                r = requests.get(self.url)
                 #数据进行切片分割
                 content = r.text.split('{')[1].split('}')[0].split(',')
                 if self.dumpFlag: print content
@@ -38,7 +37,7 @@ class DataScrape():
                     infoDict = dict(zip(tag,value))
 
                 if self.dumpFlag: print infoDict
-
+                # 用"最新价( b )"刷新各周期数据库
                 retList = [infoDict[u'"p"'],infoDict[u'"b"'],infoDict[u'"h"'],infoDict[u'"l"'],\
                         time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(float(infoDict[u'"t"'])))]
             except (Exception),e:
