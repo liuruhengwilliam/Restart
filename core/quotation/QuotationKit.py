ee28d4045d8650ffd592b86cd6c09c14886f5682
liuruheng <liuruheng@seuic.com>
2017-11-13 16:18:23 +0800

    添加db文件转csv文件的工具函数

--- core/quotation/QuotationKit.py
+++ core/quotation/QuotationKit.py
@@ -2,39 +2,53 @@
 
 import csv
 import sqlite3
-from resource import Primitive
+from core.resource import Configuration
+from core.resource import Primitive
+from core.resource import Trace
 
-def translate_db_into_csv(filePath, lineCnt):
-    """ 外部接口API:将db文件转换成csv文件
-        filePath: db文件名（含文件路径）
-        lineCnt: 截取db条目数目
+def translate_db_into_csv(dbFile, lineCnt):
+    """ 外部接口API:将db文件转换成同名同路径的csv文件
+        dbFile: db文件名（含文件路径）
+        lineCnt: 截取db条目数目。注：‘-1’表示全部转换。
     """
-    csvFile = file(filePath.split('.')[0] + '.csv', 'wb')
+    csvFile = file(dbFile.split('.')[0] + '.csv', 'wb')
     csvWriter = csv.writer(csvFile, dialect='excel')
 
+    # 写入抬头信息
+    title = ['id'] + map(lambda x:x , Configuration.QUOTATION_STRUCTURE)
+    csvWriter.writerow(title)
+
     # db文件操作
-    db = sqlite3.connect(filePath)
+    db = sqlite3.connect(dbFile)
     dbCursor = db.cursor()
     try:
-        results = dbCursor.execute(Primitive.QUOTATION_DB_QUERY)
-        ret = results.fetchall()
+        results = dbCursor.execute(Primitive.QUOTATION_DB_QUERY_DESC)# 倒序方式查询
+        if lineCnt == -1: # 获取所有条目
+            ret = results.fetchall()
+        else:# 获取指定数量的最近条目
+            ret = results.fetchmany(lineCnt)
         # db查询条目插入到csv文件中。后续可以根据lineCnt进行截取。
         for item in ret:
             csvWriter.writerow(item)
     except (Exception),e:
-        print " translate quotation Exception: " + e.message
+        Trace.output("fatal", " translate quotation Exception: " + e.message)
     csvFile.close()
     db.commit()
     dbCursor.close()
     db.close()
 
-def tranlate_list_into_csv(fileName,listToDeal):
+def tranlate_list_into_csv(listToDeal,csvFileName):
     """ 外部接口API:将列表转换成csv文件
-        fileName: csv文件名（含文件路径）
         listToDeal: 列表
+        csvfileName: csv文件名（含文件路径）
     """
-    csvFile = file(fileName, 'wb')
+    csvFile = file(csvFileName, 'wb')
     csvWriter = csv.writer(csvFile, dialect='excel')
+
+    # 写入抬头信息
+    title = ['id'] + map(lambda x:x , Configuration.QUOTATION_STRUCTURE)
+    csvWriter.writerow(title)
+
     for item in listToDeal:
         csvWriter.writerow(item)
     csvFile.close()
