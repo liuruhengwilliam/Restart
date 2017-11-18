4a4c6d71d0724bcc6bd1f809d70cc87777705088
liuruheng <joeeheng@163.com>
2017-11-18 21:27:10 +0800

    调整辅助工具类

--- core/assistant.py
+++ core/assistant.py
@@ -6,6 +6,12 @@ import os
 from resource import Configuration
 from quotation import QuotationKit
 
+"""
+    辅助类：
+    1.db文件或db条目转换成csv文件的方法；
+    2.画图方法；
+"""
+
 def db_to_csv(file,cnt):
     """ db文件转换成csv文件的工具函数
         file: db文件名（含文件路径）。raw_input方式接收控制台输入，字符串类型。
@@ -20,6 +26,10 @@ def db_to_csv(file,cnt):
     QuotationKit.translate_db_into_csv(fileName,int(cnt))
 
 if __name__ == '__main__':
-    filename = raw_input("file name input: ")
-    cnt = raw_input("cnt to be translated: ")
-    db_to_csv(filename,cnt)
+    choiceIndex = raw_input("Please choose:\n"+"1.db translate to csv\n"+"2.drawing plotting\n"+"Your choice:\n")
+    if choiceIndex == '1':
+        filename = raw_input("file name input: ")
+        cnt = raw_input("cnt to be translated: ")
+        db_to_csv(filename,cnt)
+    else:
+        print("drawing")
