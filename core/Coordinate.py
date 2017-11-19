017b79bfd25ab78632ae76be9e94787331b7962c
liuruheng <joeeheng@163.com>
2017-11-19 19:31:48 +0800

    相关功能模块接口调整

--- core/Coordinate.py
+++ core/Coordinate.py
@@ -11,7 +11,10 @@ from scrape.DataScrape import *
 from quotation.QuotationDB import *
 from quotation.QuotationRecord import *
 
-class CoordinateDS2QDB():
+class Coordinate():
+    """
+        协作类:衔接“数据抓取”、“行情数据库”、“策略算法”模块，协同工作。
+    """
     def __init__(self):
         self.week = (datetime.datetime.now()).strftime('%U')# 本周周数记录
 
@@ -25,14 +28,17 @@ class CoordinateDS2QDB():
                                           self.recordHdl.get_record_dict())
 
     def init_quotation(self):
-        """ 外部接口API:行情数据库线程准备 """
+        """ 外部接口API:行情数据库准备 """
         # 创建记录字典
         self.recordHdl.create_record_dict()
         # 创建行情数据库文件
         self.dbQuotationHdl.create_period_db(Configuration.get_working_directory())
 
+    def init_earnrate(self):
+        """ 外部接口API：初始化胜率数据库 """
+
     # 以下是定时器回调函数:
-    def work_DS2QDB_heartbeat(self):
+    def work_heartbeat(self):
         """ 快速定时器(心跳定时器)回调函数 : 数据抓取模块和行情数据库线程(缓冲字典)之间协同工作函数 """
         # 全球市场结算期间不更新缓冲记录
         if Constant.is_closing_market():
@@ -45,8 +51,8 @@ class CoordinateDS2QDB():
         if len(infoList) != 0:
             self.recordHdl.update_dict_record(infoList)
 
-    def work_DS2QDB_operate(self):
-        """ 慢速定时器组回调函数 : 更新行情数据库 """
+    def work_operation(self):
+        """ 慢速定时器组回调函数 : 更新行情数据库和策略算法计算 """
         # 全球市场结算时间不更新数据库
         if Constant.is_closing_market():
             return
@@ -54,3 +60,5 @@ class CoordinateDS2QDB():
             sys.exit()
 
         self.dbQuotationHdl.update_period_db()
+
+        # 各周期定时器到期之后，可根据需求调用策略算法模块的接口API对本周期数据进行计算。
--- core/Start.py
+++ core/Start.py
@@ -4,28 +4,28 @@ from resource import Misc
 Misc.misc_init()#初始化杂项
 
 import threading
-from CoordinateDS2QDB import *
+from Coordinate import *
 from timer.TimerMotor import TimerMotor
 from resource import Configuration
 
 def main():
     """执行模块"""
     # 数据抓取和行情数据库相关初始化
-    core4DS = CoordinateDS2QDB()
+    core4DS = Coordinate()
 
     core4DS.init_quotation()
 
     funcList = [] # 回调函数列表
     # 数据抓取模块和行情数据库模块挂载到周期定时器
-    funcList.append(core4DS.work_DS2QDB_heartbeat)
-    funcList.extend([core4DS.work_DS2QDB_operate]*10)
+    funcList.append(core4DS.work_heartbeat)
+    funcList.extend([core4DS.work_operation]*10)
 
     timerMotorHdl = TimerMotor()
     # 初始化固定周期定时器
     timerMotorHdl.init_fasten_timer(funcList, Configuration.QUOTATION_DB_PERIOD)
 
     #ER数据库
-    #......
+    #timerMotorHdl.init_chain_timer(...,Configuration.CHAIN_PERIOD)
 
     # 定时器线程启动
     timerMotorHdl.start_timer()
