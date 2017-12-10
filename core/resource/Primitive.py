48d948a5f61b3b1371681d3da8de0e395709c400
liuruheng <joeeheng@163.com>
2017-12-10 20:45:42 +0800

    盈亏数据库结构调整

--- core/resource/Primitive.py
+++ core/resource/Primitive.py
@@ -18,18 +18,19 @@ QUOTATION_DB_QUERY_DESC = 'select * from quotation order by id desc'
 #=================================================================================
 # 盈利数据库操作原语
 # 建表
+# 表结构说明：
+# 策略点方向，策略点价格，策略点时间，技术指标名称（组合图形名称索引）
+# 策略点各周期决策值：（5min or 15min or 30min or 1hour or 2hour or 4hour or 6hour or 12hour or 1day or 1week）
+# 策略点后盈亏值的时间统计:（最大值及时间/最小值及时间/5min/15min/30min/1hour/2hour/4hour/6hour/12hour/1day/1week）
 EARNRATE_DB_CREATE = 'create table earnrate(\
     id integer primary key autoincrement not null default 1,\
-    direction int, price float,time float,\
-    M5Dtrm int, M15Dtrm int, M30Dtrm int, H1Dtrm int, H2Dtrm int,\
-    H4Dtrm int, H6Dtrm int, H12Dtrm int, D1Dtrm int, W1Dtrm int,\
-    maxEarn float, maxEarnTime float, minEarn float,minEarnTime float,\
+    direction int, price float,time float, patternName text,\
+    Dtrm int, maxEarn float, maxEarnTime float, minEarn float,minEarnTime float,\
     M5Earn float, M15Earn float, M30Earn float, H1Earn float, H2Earn float,\
     H4Earn float, H6Earn float, H12Earn float, D1Earn float, W1Earn float);'
 
 # 插入
-EARNRATE_DB_INSERT = 'insert into earnrate(direction,price,time,M5Dtrm,M15Dtrm,M30Dtrm,H1Dtrm,\
-    H2Dtrm,H4Dtrm,H6Dtrm,H12Dtrm,D1Dtrm,W1Dtrm) values(?,?,?,?,?,?,?,?,?,?,?,?,?)'
+EARNRATE_DB_INSERT = 'insert into earnrate(direction,price,time,patternName,Dtrm) values(?,?,?,?,?)'
 
 def query_earnrate_db(column,id = ''):
     """ 外部接口API：根据数据库条目id查找相应记录项
