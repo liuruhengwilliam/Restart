e124bbf202fc0d5d7eab6ee2f6158196256dfac3
liuruheng <liuruheng@seuic.com>
2017-12-15 18:07:31 +0800

    增加talib库K线模式定时检测功能

---
+++ core/strategy/StrategyMisc.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+import numpy as np
+
+from resource import Constant
+from quotation import QuotationKit
+
+def process_quotes_candlestick_pattern(index,file,dataWithID):
+    """ 外部接口API:蜡烛图组合模式或图形的预处理函数
+        index:周期序列的下标
+        file：数据库文件全路径（包含文件名，蜡烛图文件保存路径同目录）
+        dataWithID: dataframe结构的数据
+        返回值：dateframe结构数据
+    """
+    dataCnt = dataWithID.iloc[-1:]['id']
+    gap = Constant.STRATEGY_CANDLESTICK_CNT[index] - int(dataCnt)
+    if gap > 0:# 要补齐蜡烛图中K线数目
+        dataWithID = QuotationKit.supplement_quotes(file,dataWithID,int(gap))
+
+    #dataWithID.drop(dataWithID.columns[0:1], axis=1, inplace=True)#抛弃'id'栏
+
+    return dataWithID
--- core/strategy/strategy.py
+++ core/strategy/strategy.py
@@ -1,39 +1,44 @@
 #coding=utf-8
 
+import sys
 import talib
+import traceback
 import numpy as np
 import pandas as pd
+import StrategyMisc
+from resource import Constant
+from resource import Configuration
 
-from quotation import QuotationKit
 
 """
     策略算法模块：提供策略算法接口API
 """
 
-def talib_func():
-    func_collection = talib.get_functions()
-    for i in func_collection:
-        print i
+def check_candlestick_pattern(indx,file,dataWithId):
+    """ 外部接口API: 蜡烛图组合图形的识别
+    """
+    dataDealed = StrategyMisc.process_quotes_candlestick_pattern(indx,file,dataWithId)
 
-    func_dict = talib.get_function_groups()
-    # 迭代器
-    for item in func_dict.iteritems():
-        print item
+    for pattern in Constant.STRATEGY_CANDLESTICK:# 遍历蜡烛图组合模型
+        try:
+            result = getattr(talib, pattern)(dataDealed['open'].values,dataDealed['high'].values,\
+                                             dataDealed['low'].values,dataDealed['close'].values)
+            # result是numpy.ndarray数据结构
+        except (Exception),e:
+            exc_type,exc_value,exc_tb = sys.exc_info()
+            traceback.print_exception(exc_type, exc_value, exc_tb)
+            traceback.print_exc(file=open(Configuration.get_working_directory()+'trace.txt','a'))
+        finally:
+            if len(result)!=0 and result.any()==True:
+                #print Constant.QUOTATION_DB_PREFIX[indx]+' period '+pattern,result.max(),result.min()
+                dataDealed[pattern] = result
 
-def numpy_data():
-    data_analyse = numpy.random.random(10)
-    print type(data_analyse)
+    dataDealed.to_csv(pattern+'.csv',encoding='utf-8')
 
-def talib_sma_test():
-    data_analyse = {
-        'open':numpy.random.random(100),
-        'high':numpy.random.random(100),
-        'low':numpy.random.random(100),
-        'close':numpy.random.random(100),
-        'volume':numpy.random.random(100)
-    }
-    #close = numpy.random.random(100)
-    #output = talib.SMA(close)
-    output = talib.abstract.SMA(data_analyse, timeperiod=20, price='open')
-    print (output)
-
+def check_strategy(indx,file,dataWithId):
+    """ 外部接口API: 检测行情，依据策略生成相关指令
+        index:周期序列的下标
+        file: 行情数据库文件(包含文件路径名)
+        dataWithID: 来自行情数据库的dataframe结构数据
+    """
+    check_candlestick_pattern(indx,file,dataWithId)
