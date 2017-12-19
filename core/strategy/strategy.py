0deec3f3fa41f8a16b4ce2102ab9851534a8364a
liuruheng <liuruheng@seuic.com>
2017-12-19 18:19:44 +0800

    依据新工作目录调整策略生成模块的函数调用

--- core/strategy/strategy.py
+++ core/strategy/strategy.py
@@ -2,7 +2,6 @@
 
 import sys
 import talib
-import platform
 import traceback
 import numpy as np
 import pandas as pd
@@ -14,10 +13,11 @@ from resource import Configuration
 """
     策略算法模块：提供策略算法接口API
 """
-def check_candlestick_pattern(indx,file,dataWithId):
+def check_candlestick_pattern(periodName,dataWithId):
     """ 外部接口API: 蜡烛图组合图形的识别
+        periodName: 周期名称的字符串
     """
-    dataDealed = StrategyMisc.process_quotes_candlestick_pattern(indx,file,dataWithId)
+    dataDealed = StrategyMisc.process_quotes_candlestick_pattern(periodName,dataWithId)
 
     for pattern in Constant.STRATEGY_CANDLESTICK:# 遍历蜡烛图组合模型
         result = None
@@ -29,12 +29,8 @@ def check_candlestick_pattern(indx,file,dataWithId):
             # result是numpy.ndarray数据结构
             if len(result)!=0 and result.any()==True:
                 dataCsv[pattern] = result # 增加蜡烛图组合模式的名称列
-                if (platform.system() == "Windows"):
-                    csvFile = '%s%s%s-%s.csv'%(file.split('.')[0],'\\',Constant.QUOTATION_DB_PREFIX[indx],pattern)
-                else:
-                    csvFile = '%s%s%s-%s.csv'%(file.split('.')[0],'/',Constant.QUOTATION_DB_PREFIX[indx],pattern)
-
-                dataCsv[dataCsv[pattern]!=0].to_csv(csvFile,encoding='utf-8')
+                csvFile=Configuration.get_period_working_folder(periodName)+'%s-%s.csv'%(periodName,pattern)
+                dataCsv[dataCsv[pattern]!=0].to_csv(csvFile,encoding='utf-8')#转存csv文件
 
         except (Exception),e:
             exc_type,exc_value,exc_tb = sys.exc_info()
@@ -42,14 +38,14 @@ def check_candlestick_pattern(indx,file,dataWithId):
             traceback.print_exc(file=open(Configuration.get_working_directory()+'trace.txt','a'))
 
 
-def check_strategy(indx,file,dataWithId):
+def check_strategy(periodName,dataWithId):
     """ 外部接口API: 检测行情，依据策略生成相关指令
-        index:周期序列的下标
-        file: 行情数据库文件(包含文件路径名)
+        periodName:周期名称字符串
         dataWithID: 来自行情数据库的dataframe结构数据
     """
+    indx = Constant.QUOTATION_DB_PREFIX.index(periodName)
     # 微尺度周期不匹配蜡烛图模式（减少策略生成的频度）
     if Constant.SCALE_CANDLESTICK[indx] < Constant.DEFAULT_SCALE_CANDLESTICK_PATTERN:
         return
 
-    check_candlestick_pattern(indx,file,dataWithId)
+    check_candlestick_pattern(periodName,dataWithId)
