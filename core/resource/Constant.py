0906eb58a50fc01423792f0a561b64cd776d0581
liuruheng <liuruheng@seuic.com>
2018-03-13 11:06:42 +0800

    软件版本号升级为V1.2.0

--- core/resource/Constant.py
+++ core/resource/Constant.py
@@ -20,7 +20,7 @@ def get_date_code():
     month,day = dt.strftime('%m'),dt.strftime('%d')
     return MONTH_CODE[int(month)-1]+DAY_CODE[int(day)-1]
 
-VERSION_CODE = 'V1.1.4'
+VERSION_CODE = 'V1.2.0'
 def get_version_info():
     """ 内/外部接口API: """
     return VERSION_CODE + get_date_code() + "\n" + \
