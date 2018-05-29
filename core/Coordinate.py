eb8313631069b1549ead8db82b8e293a3bc6c42e
liuruheng <liuruheng@seuic.com>
2018-05-29 14:20:13 +0800

    周期行情数据更新定时器的主处理函数接口适配

--- core/Coordinate.py
+++ core/Coordinate.py
@@ -64,7 +64,7 @@ class Coordinate():
                 continue
 
         markQuery = datetime.datetime.now()
-        Trace.output('info', "It cost: %s to query stock(%s) from EastMoney."%\
+        Trace.output('debug', "It cost %s to query target(%s)."%\
                      (str(markQuery-markStart),' '.join(self.recordHdl.get_target_list())))
 
     def work_operation(self):
@@ -75,50 +75,53 @@ class Coordinate():
         year,week = markStart.strftime('%Y'),markStart.strftime('%U')
         for target in self.recordHdl.get_target_list():
             # 更新各周期行情数据缓存
-            quoteGenerator = self.quoteHdl.update_quote(target)
-            quoteDF = quoteGenerator.next()
+            quoteDF = self.quoteHdl.update_quote(target)
             quoteDF.to_csv(Configuration.get_working_directory()+target+'-%s-%s-quote.csv'%(year,week),\
                             columns=['period',]+list(Constant.QUOTATION_STRUCTURE))
 
             # 按照时间次序排列，并删除开头的十一行（实时记录行）
-            quoteDF = quoteDF.iloc[len(Constant.QUOTATION_DB_PERIOD):]
+            quoteFilterDF = quoteDF.iloc[len(Constant.QUOTATION_DB_PERIOD):]
 
-            # 各周期进行测验。若到期，则进行策略匹配。
-            for index,period in enumerate(Constant.QUOTATION_DB_PREFIX[1:]):
+            # 各周期进行测验。若到期，则进行策略更新/匹配。
+            for index,period in enumerate(Constant.QUOTATION_DB_PREFIX):
+                if index < Constant.QUOTATION_DB_PERIOD.index(Constant.UPDATE_BASE_PERIOD):
+                    continue
                 if self.quoteHdl.remainder_higher_order_tm(period)!=0:
                     continue
 
                 # 策略盈亏率数据库操作。先进行统计更新策略盈亏率数据，然后再分析及插入新条目。
                 # 5min周期定时器的主要任务就是更新盈亏率数据库。但5min行情数据必须周期刷新，所以update_quote(period)要前置。
                 if period == '5min':
-                    recInfo = self.recordHdl.get_record_dict(target)
-                    self.strategy.update_strategy([recInfo['time'],recInfo['high'],recInfo['low']])
+                    self.strategy.update_strategy([quoteDF.time[index],quoteDF.high[index],quoteDF.low[index]])
                     markEnd5min = datetime.datetime.now()
-                    Trace.output('info', "As for %s,Period %s update strategy cost: %s\n"\
+                    Trace.output('info', "As for %s,Period %s update strategy cost: %s"\
                              %(target, period, str(markEnd5min-markStart)))
                     continue
 
+                if len(quoteFilterDF) == 0:#若无记录，则跳出循环
+                    break
+
+                quotePeriodDF = quoteFilterDF[quoteFilterDF['period']==period]
                 # 指标计算和记录
-                self.indicator.process_indicator(period,quoteDF[quoteDF['period'==period]])
+                self.indicator.process_indicator(quotePeriodDF)
                 markIndicator = datetime.datetime.now()
                 Trace.output('info', "As for %s,Period %s process indicator cost: %s"\
-                             %(target, period, str(markIndicator-markStart)))
+                            %(target, period, str(markIndicator-markStart)))
 
                 # 策略算法计算
                 # 数据加工补全
-                dataDealed = StrategyMisc.process_quotes_candlestick_pattern\
-                    (Configuration.get_period_working_folder(period),quoteDF[quoteDF['period'==period]])
-                self.strategy.check_strategy(period,dataDealed)
+                dataDealed = StrategyMisc.process_quotes_candlestick_pattern(quotePeriodDF)
+                self.strategy.check_strategy(dataDealed)
                 markStrategy = datetime.datetime.now()
-                Trace.output('info', "As for %s,Period %s check strategy cost: %s\n"\
-                             %(target, period, str(markStrategy-markIndicator)))
+                Trace.output('info', "As for %s,Period %s check strategy cost: %s"\
+                            %(target, period, str(markStrategy-markIndicator)))
 
         # 基准定时器计数自增
         self.quoteHdl.increase_timeout_count()
 
         markEnd = datetime.datetime.now()
-        Trace.output('info', "It cost %s to operate stock(%s) with timing out at %s."%\
-                     (str(markEnd-markStart),' '.join(self.recordHdl.get_target_list())))
+        Trace.output('info', "It cost %s to operate target(%s) at %s"%\
+                     (str(markEnd-markStart),' '.join(self.recordHdl.get_target_list()),markStart))
 
     def statistics_settlement(self):
         """内部接口API: 盈亏统计工作。由汇总各周期盈亏数据库生成表格文件。"""
