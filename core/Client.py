fae864c3cc3b849852b538ac0fa847f745c5b148
liuruheng <liuruheng@seuic.com>
2018-06-14 13:49:00 +0800

    客户端程序支持多目录分析功能

--- core/Client.py
+++ core/Client.py
@@ -4,29 +4,27 @@ from timer import TimerMotor
 from resource import Constant
 from resource import Trace
 from resource import Configuration
-from strategy.ClientMatch import ClientMatch
+from statistics.Statistics import Statistics
 
 Configuration.create_working_directory() #创建工作目录
 
 def client_main():
-    #在Linux平台下可以利用while命令循环执行shell命令
-    #参考文档：http://blog.csdn.net/daoshuti/article/details/72831256
-    if Constant.is_closing_market():
-        return
-    if Constant.is_weekend():
-        os._exit(0) #退出Python程序
-
-    path = Configuration.get_property("dependDataPath")
-    if path == None:
+    # 依赖目录可能有若干个
+    pathTotal = Configuration.get_property("dependDataPath")
+    if pathTotal == None:
         Trace.output('fatal','no data path depended!')
         return
-    clientMatchHdl = ClientMatch(path)
 
     #也可使用配置文件设置循环定时器的方式
     clientPeriod = Configuration.get_property("clientPeriod")#单位是秒
     if clientPeriod == None:
         clientPeriod = Constant.QUOTATION_DB_PERIOD[1]
-    TimerMotor.start_loop_timer((clientMatchHdl.match_KLineIndicator,),[int(clientPeriod),])
+
+    #在Linux平台下可以利用while命令循环执行shell命令
+    #参考文档：http://blog.csdn.net/daoshuti/article/details/72831256
+    for path in pathTotal.split(';'):
+        statisticHdl = Statistics(path)
+        TimerMotor.start_loop_timer((statisticHdl.statistics_operation,),[int(clientPeriod),])
 
 if __name__ == '__main__':
     client_main()
