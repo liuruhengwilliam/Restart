cd862e198be90d7698ee31d4a408ac8370e7a1e6
liuruheng <liuruheng@seuic.com>
2018-06-19 11:27:53 +0800

    py2exe生成GUI示例程序及方法

--- core/setup.py
+++ core/setup.py
@@ -1,7 +1,13 @@
 from distutils.core import setup
 import py2exe
+import glob
+data_files = [(r'mpl-data', glob.glob(r'C:\ProgramData\Anaconda2\Lib\site-packages\matplotlib\mpl-data\*.*')),
+              (r'mpl-data', [r'C:\ProgramData\Anaconda2\Lib\site-packages\matplotlib\mpl-data\matplotlibrc']),
+              (r'mpl-data\images',glob.glob(r'C:\ProgramData\Anaconda2\Lib\site-packages\matplotlib\mpl-data\images\*.*')),
+              (r'mpl-data\fonts',glob.glob(r'C:\ProgramData\Anaconda2\Lib\site-packages\matplotlib\mpl-data\fonts\*.*'))]
+
 setup(
     version='2.1.5',
     description='Project RESTART. Marked by liuruheng on 2017-11-13',
     name = 'alpha V2.1.5',
-    console=['Stock.py'])
+    windows=[r'test/assistant.py'],data_files=data_files)
--- core/test/assistant.py
+++ core/test/assistant.py
@@ -5,12 +5,6 @@ import platform
 import sys
 import os
 import pandas as pd
-#from resource import Constant
-#from resource import Configuration
-#from resource import DataSettleKit
-#from indicator import CandleStick
-#from strategy.ClientMatch import ClientMatch
-#from strategy.Strategy import Strategy
 
 class DirDialog(wx.Frame):
     """"""
@@ -31,47 +25,8 @@ class DirDialog(wx.Frame):
         dlg.Destroy()
 
 if __name__ == '__main__':
-    frame = wx.PySimpleApp()
+    frame = wx.App()
     app = DirDialog()
     app.Show()
     frame.MainLoop()
 
-"""
-    辅助类：
-    1.画图方法；
-    2.历史数据分析；
-"""
-
-def old_API():
-    choiceIndex = raw_input("Please choose:\n"+"  1.drawing picture of indicator\n"\
-                            +"  2.analyse quote data in history\n"\
-                            +"  3.translate quote database to csv file\n"\
-                            +"Your choice:\n")
-    if choiceIndex == '1':
-        filename = raw_input("file name input: ")
-        periodName = raw_input("period name input:")
-        if Constant.QUOTATION_DB_PREFIX.count(periodName) == 0:
-            print "Error period name!"
-            sys.exit()
-        if filename.find('.csv') != -1:
-            target=Configuration.get_field_from_string(filename)[-1].split('-')[0]
-            data = pd.read_csv(filename)
-            data = data.iloc[len(Constant.QUOTATION_DB_PERIOD):]
-            CandleStick.manual_show_candlestick(target,periodName,data[data['period']==periodName])
-        else:
-            print "Error file name input!"
-    elif choiceIndex == '2':
-        path = raw_input("Please input folder path WITH THE END OF '\\': ")
-        if path=='':
-            path = Configuration.get_working_directory()
-        clientMatchHdl = ClientMatch(path)
-        clientMatchHdl.match_KLineIndicator()
-    elif choiceIndex == '3':
-        filePath = raw_input("Please input full path of database file: ")
-        ret = DataSettleKit.translate_db_into_csv(filePath)
-        if ret == False:
-            print "Failed to translate file:%s"%filePath
-        else:
-            print "Completed Successfully!"
-    else:
-        print "Error choice!"
