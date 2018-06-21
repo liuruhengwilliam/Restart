d43bfca8b06c0a175935d5048d76a8375f5f9a09
liuruheng <liuruheng@seuic.com>
2018-06-21 10:50:59 +0800

    增加蜡烛图文件绘制完成之后指定保存路径功能

--- core/indicator/CandleStick.py
+++ core/indicator/CandleStick.py
@@ -20,7 +20,7 @@ from resource import Configuration
 from indicator import MA
 from indicator import BollingerBands
 
-def show_candlestick(target,dfData, ma, BBands, periodName):
+def show_candlestick(target,dfData, ma, BBands, periodName,path=None):
     """ 内部接口API:
         dfData: Dataframe数据接口。
         ma： 移动平均线数据列表
@@ -70,14 +70,17 @@ def show_candlestick(target,dfData, ma, BBands, periodName):
         plt.ylabel("Price ($)")
         # 生成图片文件保存
         timestamp = datetime.datetime.now().strftime('%b%d_%H_%M')
-        plt.savefig('%s%s-%s-%s.png'%(Configuration.get_working_directory(),target,periodName,timestamp),dpi=200)
+        if path is None:
+            plt.savefig('%s%s-%s-%s.png'%(Configuration.get_working_directory(),target,periodName,timestamp),dpi=200)
+        else:
+            plt.savefig('%s%s-%s-%s.png'%(path,target,periodName,timestamp),dpi=200)
         #plt.show() #只有主线程才能处理该信号 -- ValueError: signal only works in main thread
     except (Exception),e:
         exc_type,exc_value,exc_tb = sys.exc_info()
         traceback.print_exception(exc_type, exc_value, exc_tb)
         traceback.print_exc(file=open(Configuration.get_working_directory()+'trace.txt','a'))
 
-def manual_show_candlestick(target,periodName,data):
+def manual_show_candlestick(target,periodName,data,path):
     """ 外部接口API: 手动绘图使用。
         periodName:周期名称的字符串（用于计算蜡烛图展示根数）
         data:行情数据库中dateframe结构的数据。
@@ -92,4 +95,4 @@ def manual_show_candlestick(target,periodName,data):
     # 布林线
     bbands = BollingerBands.compute_BBands(dataPicked['close'].as_matrix())
 
-    show_candlestick(target,dataPicked,ma,bbands,periodName)
+    show_candlestick(target,dataPicked,ma,bbands,periodName,path)
