1b2a79d81349a7e0f2f940a65fa6a9a719540b30
liuruheng <liuruheng@seuic.com>
2018-07-10 17:23:29 +0800

    Cython适配层模块代码调整

--- core/Coordinate.py
+++ core/Coordinate.py
@@ -12,8 +12,8 @@ from quotation.Quotation import *
 from quotation.QuotationRecord import *
 from quotation.QuoteUpdate import update_quote
 from strategy.Strategy import Strategy
-from strategy import StrategyMisc
-from indicator import CandleStick
+from strategy.StrategyUpdate import update_strategy
+from strategy.StrategyCheck import check_strategy
 
 class Coordinate():
     """
@@ -29,7 +29,6 @@ class Coordinate():
         self.indicator = Indicator()
         # 策略类初始化
         self.strategy = Strategy(self.recordHdl.get_target_list())
-
         Trace.output('info', " ==== ==== Server Complete Initiation and Run Routine ==== ==== \n")
 
     # 以下是定时器回调函数:
@@ -71,6 +70,7 @@ class Coordinate():
             挂载在基准定时器。根据倍数关系，驱动更新其他大周期行情数据缓存。
         """
         markStart = datetime.datetime.now()
+        baseTmCnt = self.quoteHdl.get_baseTM_cnt()
         for target in self.recordHdl.get_target_list():
             if target == '':#分解出的异常字符
                 continue
@@ -80,63 +80,46 @@ class Coordinate():
                 return
 
             markStartTarget = datetime.datetime.now()
+
             # 更新各周期行情数据缓存
-            record = self.recordHdl.get_record_dict(target)
             quoteDF = self.quoteHdl.get_quote(target)
-            baseTmCnt = self.quoteHdl.get_baseTM_cnt()
-            quoteDF = update_quote(target,record,quoteDF,baseTmCnt)
+            record = self.recordHdl.get_record_dict(target)
+            quoteDF = update_quote(record,quoteDF,baseTmCnt)
             # 更新之后要回写行情数据
             self.quoteHdl.set_quote(target,quoteDF)
             self.recordHdl.reset_target_record(target)
+
+            markQuoteUpd = datetime.datetime.now()
+            Trace.output('debug',"For %s,update quote cost:%s"%(target,str(markQuoteUpd-markStartTarget)))
+
             if len(quoteDF) <= len(Constant.QUOTATION_DB_PERIOD):#无附着条目直接返回
                 continue
-            # 按照时间次序排列，并删除开头的十一行（实时记录行）
-            quoteFilterDF = quoteDF.iloc[len(Constant.QUOTATION_DB_PERIOD):]
 
-            # 各周期进行测验。若到期，则进行策略更新/匹配。
-            for index,period in enumerate(Constant.QUOTATION_DB_PREFIX):
-                if index < Constant.QUOTATION_DB_PERIOD.index(Constant.UPDATE_BASE_PERIOD):
-                    continue
-                if self.quoteHdl.remainder_higher_order_tm(period)!=0:#未到期
-                    continue
-                # 异常数据对应的周期不更新/匹配
-                if quoteDF.time[index] == ' ':
-                    Trace.output('warn','%s skip period %s for strategy with zero record'%(target,period))
-                    continue
+            # 在基础更新定时器到期时更新策略记录条目
+            baseTmIndx = Constant.QUOTATION_DB_PERIOD.index(Constant.UPDATE_BASE_PERIOD)
+            infoList = [quoteDF.time[baseTmIndx],quoteDF.high[baseTmIndx],quoteDF.low[baseTmIndx]]
+            dfStrategy = update_strategy(self.strategy.get_strategy(target),infoList)
 
-                # 策略盈亏率数据库操作。先进行统计更新策略盈亏率数据，然后再分析及插入新条目。
-                # 基准更新定时器的主要任务就是更新盈亏率数据库。
-                if index == Constant.QUOTATION_DB_PERIOD.index(Constant.UPDATE_BASE_PERIOD):
-                    infoList = [quoteDF.time[index],quoteDF.high[index],quoteDF.low[index]]
-                    self.strategy.update_strategy(target,infoList)
-                    markEnd5min = datetime.datetime.now()
-                    Trace.output('debug', "As for %s,Period %s update strategy cost: %s"\
-                             %(target, period, str(markEnd5min-markStartTarget)))
-                    continue
+            markStrgUpd = datetime.datetime.now()
+            Trace.output('debug',"For %s,update strategy cost:%s"%(target,str(markStrgUpd-markQuoteUpd)))
 
-                quotePeriodDF = quoteFilterDF[quoteFilterDF['period']==period]#按周期挑选条目
-                if len(quotePeriodDF) == 0:#若无记录，则无法进行模式匹配。跳出循环。
-                    break
+            # 策略匹配并生成条目(若有)
+            dfRet = check_strategy(baseTmCnt,quoteDF)
+            # 策略新条目要回写
+            self.strategy.set_strategy(target,dfRet)
 
-                # 指标计算和记录
-                self.indicator.process_indicator(quotePeriodDF)
-                markIndicator = datetime.datetime.now()
-
-                # 策略算法计算
-                dataDealed = StrategyMisc.process_quotes_candlestick_pattern(quotePeriodDF)
-                self.strategy.check_strategy(target,dataDealed)
-                markStrategy = datetime.datetime.now()
-                Trace.output('debug', "As for %s,Period %s check strategy cost: %s"\
-                            %(target, period, str(markStrategy-markIndicator)))
+            markStrgChk = datetime.datetime.now()
+            Trace.output('debug',"For %s,check strategy cost:%s"%(target,str(markStrgChk-markStrgUpd)))
 
             if re.search(r'[^a-zA-Z]',target) is None:#股票转存由专门的线程负责，不再这里处理。
-                if self.quoteHdl.remainder_higher_order_tm('1hour')==0:#到期
+                # 每小时存储一次
+                if baseTmCnt%(Constant.QUOTATION_DB_PERIOD[4]/Constant.UPDATE_BASE_PERIOD)==0:
                     self.storage_data(target)#转存数据到csv文件
         # 基准定时器计数自增
-        self.quoteHdl.increase_timeout_count()
+        self.quoteHdl.increase_baseTM_cnt()
 
         markEnd = datetime.datetime.now()
-        Trace.output('info', "It cost %s to operate target(%s) at %s"%\
+        Trace.output('info', "It totally cost %s to operate target(%s) from %s"%\
                      (str(markEnd-markStart),' '.join(self.recordHdl.get_target_list()),markStart))
 
     def work_storage(self):
@@ -146,7 +129,9 @@ class Coordinate():
         for target in self.recordHdl.get_target_list():
             if re.search(r'[^0-9](.*)',target) is None:#股票类型全是数字
                 # 股票类型数据较多，需要加以控制。拟定每半小时存储一次。
-                if self.quoteHdl.remainder_higher_order_tm('30min')!=0 and Constant.is_closed(target)==False:
+                baseTmCnt = self.quoteHdl.get_baseTM_cnt()
+                if baseTmCnt%(Constant.QUOTATION_DB_PERIOD[3]/Constant.UPDATE_BASE_PERIOD)!=0 \
+                        and Constant.is_closed(target)==False:
                     #在非结算期中未到期不记录
                     return
 
