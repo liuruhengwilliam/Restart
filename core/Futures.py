c3596f155bad3745615f214486656e71989ba45d
liuruheng <liuruheng@seuic.com>
2018-07-27 18:40:28 +0800

    相关类适配程序的队列通知机制

--- core/Futures.py
+++ core/Futures.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 
 import os
+import Queue
 from resource import Constant
 from Coordinate import *
 from timer import TimerMotor
@@ -10,16 +11,19 @@ Constant.envi_init()#初始化杂项
 
 def server_main():
     """执行模块"""
+    # 相关队列
+    inter2exterQueue = Queue.Queue()
+
     # 数据抓取和行情数据库相关初始化
-    coordinate = Coordinate()
+    coordinate = Coordinate(inter2exterQueue)
 
     # 数据抓取模块和数据库模块挂载到周期定时器
-    HBfuncHook = (coordinate.work_heartbeat,) # 心跳定时器处理回调函数
-    PeriodfuncHook = (coordinate.work_operation,coordinate.work_storage) # 一般周期定时器处理回调函数
+    HBfuncHook = (coordinate.work_heartbeat_query,) #心跳定时器处理回调函数
+    PeriodfuncHook = (coordinate.work_internal_operate,coordinate.work_external_operate)
     funcList = (HBfuncHook + PeriodfuncHook) # 回调函数列表
 
     # 周期定时器线程启动
-    TimerMotor.start_loop_timer(funcList,Constant.QUOTATION_DB_PERIOD[:3])
+    TimerMotor.start_loop_timer(funcList,Constant.QUOTATION_DB_PERIOD[0:2]+tuple([Constant.REFRESH_PERIOD]))
 
 if __name__ == '__main__':
     server_main()
--- core/Stock.py
+++ core/Stock.py
@@ -1,5 +1,7 @@
 #coding=utf-8
+
 import os
+import Queue
 from timer import TimerMotor
 from Coordinate import *
 from resource import Constant
@@ -10,21 +12,24 @@ Configuration.create_working_directory() #创建工作目录
 Constant.envi_init()#初始化杂项
 def stock_main():
     """ 股票行情模块主函数 """
+    # 相关队列
+    inter2exterQueue = Queue.Queue()
+
     #也可使用配置文件设置循环定时器的方式
     period = Configuration.get_property("stockCatchPeriod")#单位是秒
-    if period == None:# 默认为5min定时器
-        period = Constant.QUOTATION_DB_PERIOD[1]
+    if period == None:
+        period = Constant.QUOTATION_DB_PERIOD[0]
 
     # 数据抓取和行情数据库相关初始化
-    coordinate = Coordinate()
+    coordinate = Coordinate(inter2exterQueue)
 
     # 数据抓取模块和数据库模块挂载到周期定时器
-    HBfuncHook = (coordinate.work_heartbeat,) # 心跳定时器处理回调函数
+    HBfuncHook = (coordinate.work_heartbeat_query,) # 心跳定时器处理回调函数
     # 操作处理回调函数。出于减少定时器操作复杂度的考虑，只保留一个5min级别的定时器。其他周期就在5min基础上收集整理信息。
-    PeriodfuncHook = (coordinate.work_operation,coordinate.work_storage)#增加存档线程20180607
+    PeriodfuncHook = (coordinate.work_internal_operate,coordinate.work_external_operate)#增加存档线程20180607
     funcList = (HBfuncHook + PeriodfuncHook) # 回调函数列表
 
-    TimerMotor.start_loop_timer(funcList,[int(period),Constant.UPDATE_BASE_PERIOD,Constant.UPDATE_BASE_PERIOD])
+    TimerMotor.start_loop_timer(funcList,[int(period),Constant.UPDATE_BASE_PERIOD,Constant.REFRESH_PERIOD])
 
 if __name__ == '__main__':
     stock_main()
